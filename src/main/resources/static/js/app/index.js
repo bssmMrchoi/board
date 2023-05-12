d87e7a4465ed81eb841ffc03384f8dff8f8fdda0
bssmMrchoi <teacher009@bssm.hs.kr>
2023-05-12 23:39:21 +0900

    add oauth

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -42,3 +42,19 @@ function update() {
         alert(JSON.stringify(error));
     });
 }
+
+function remove() {
+    var id = $('#id').val()
+
+    $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+        }).done(function() {
+            alert("글이 삭제되었습니다.")
+            window.location.href='/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+}
