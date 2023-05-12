d266b40858ed3eeb8952112db01ee4fa7f4a7b66
bssmMrchoi <teacher009@bssm.hs.kr>
2023-05-12 10:03:15 +0900

    view get, post

--- src/main/java/com/choi/book/springboot/service/PostsService.java
+++ src/main/java/com/choi/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.choi.book.springboot.service;
 
 import com.choi.book.springboot.domain.posts.Posts;
 import com.choi.book.springboot.domain.posts.PostsRepository;
+import com.choi.book.springboot.web.dto.PostsListResponseDto;
 import com.choi.book.springboot.web.dto.PostsResponseDto;
 import com.choi.book.springboot.web.dto.PostsUpdateRequestDto;
 import com.choi.book.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,13 +25,18 @@ public class PostsService {
 
     @Transactional
     public Long update(Long id, PostsSaveRequestDto requestDto) {
-        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
     public PostsResponseDto findById(Long id) {
-        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
