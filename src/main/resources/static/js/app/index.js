105c4707fc4fa8785fa4e8ebabd8cbc3f23d9585
bssmMrchoi <teacher009@bssm.hs.kr>
2023-05-12 10:29:54 +0900

    view update

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -19,3 +19,26 @@ function save() {
         alert(JSON.stringify(error));
     });
 }
+
+function update() {
+    var data = {
+        title : $('#title').val(),
+        content : $('#content').val()
+
+    };
+
+    var id = $('#id').val()
+
+    $.ajax({
+        type: 'PUT',
+        url: '/api/v1/posts/'+id,
+        dataType: 'json',
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(data)
+    }).done(function() {
+        alert("글이 수정되었습니다.")
+        window.location.href='/';
+    }).fail(function(error) {
+        alert(JSON.stringify(error));
+    });
+}
